b67ad3b156df0339efe998cb06431f24f8f12819
SomethingFishy <69714456+Ianisop@users.noreply.github.com>
2025-04-28 10:43:05 +0200

    fixed javafx import

---
+++ pigmonitor/src/main/java/FXMLHandler.java
@@ -0,0 +1,11 @@
+import javafx.application.Application; // Importing Application class from JavaFX
+import javafx.fxml.FXMLLoader; // Importing FXMLLoader class from JavaFX
+import javafx.scene.Scene; // Importing Scene class from JavaFX
+import javafx.scene.control.Alert; // Importing Alert class from JavaFX
+import javafx.scene.control.Alert.AlertType; // Importing AlertType enum from JavaFX
+import javafx.scene.layout.AnchorPane; // Importing AnchorPane class from JavaFX
+import javafx.stage.Stage; // Importing Stage class from JavaFX
+
+
+public class FXMLHandler {
+}
